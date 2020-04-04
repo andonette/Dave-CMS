6a83eae763d2f2c66fcc2ca982de5f099ea0578f
andonette <andi@madebyfactory.com>
2020-04-04 17:58:41 +0100

    CSS & JS for front end

---
+++ blank.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Hello World!</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet">
+
+  <!-- Icons -->
+  <link href="css/nucleo-icons.css" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+  <!-- BLK• CSS -->
+  <link type="text/css" href="css/blk-design-system.min.css" rel="stylesheet">
+</head>
+<body>
+  <h1>Hello, world!</h1>
+
+  <!-- Core -->
+  <script src="js/core/jquery.min.js"></script>
+  <script src="js/core/popper.min.js"></script>
+  <script src="js/core/bootstrap.min.js"></script>
+
+  <!-- Optional plugins -->
+
+
+  <!-- BLK• JS -->
+  <script src="js/blk-design-system.js"></script>
+  </body>
+</html>
