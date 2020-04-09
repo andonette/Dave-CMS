739486c16602a526c4f474d06a704a28c1692893
andonette <andi@madebyfactory.com>
2020-04-09 17:14:35 +0100

    set category

---
+++ category.php
@@ -0,0 +1,64 @@
+<?php include 'includes/header.php' ?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8">
+            <h1 class="title">Latest Blogposts</h1>
+            <?php
+
+            if (isset($_GET['category'])) {
+                $set_category = $_GET['category'];
+            }
+
+            //query the database, select everything from the posts table
+            $query = "SELECT * FROM posts";
+            $select_all_posts_query = mysqli_query($connection, $query);
+            //create variables from the columns in the post table.
+            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                $post_id = $row['post_id'];
+                $post_title = $row['post_title'];
+                $post_author = $row['post_author'];
+                $post_date = $row['post_date'];
+                $post_image = $row['post_image'];
+                $post_content = $row['post_content'];
+                $post_author_image = $row['post_author_image'];
+                ?>
+                <div class="card card-blog card-plain blog-horizontal">
+
+                    <div class="card-image">
+                        <a href="javascript:;">
+                            <img class="img rounded" src="images/<?php echo $post_image; ?>" />
+                        </a>
+
+
+                        <div class="card-body">
+                            <h3 class="card-title">
+                                <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                            </h3>
+                            <p class="card-description">
+                                <?php echo $post_content; ?>
+                                <a href="javascript:;"> Read More </a>
+                            </p>
+                            <div class="author">
+                                <img src="images/<?php echo $post_image; ?>" alt="<?php echo $post_image; ?>" class="avatar img-raised">
+                                <div class="text">
+                                    <span class="name"><?php echo $post_title; ?></span>
+                                    <div class="meta"><?php echo $post_date; ?></div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Card -->
+            <?php } ?>
+        </div>
+        <!-- End Column -->
+        <div class="col-sm-4">
+            <?php include 'includes/sidebar.php' ?>
+        </div>
+        <!-- End Column -->
+    </div>
+    <!-- End Row -->
+</div>
+<!-- End Container -->
+<?php include 'includes/footer.php' ?>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,42 +1,48 @@
 <!-- Blog Search Well -->
-<div class="well">
-    <h4>Blog Search</h4>
-    <form class="form" action="search.php" method="post">
-        <div class="input-group">
+<div class="card">
+    <div class="card-body">
+        <h4>Blog Search</h4>
+        <form class="form" action="search.php" method="post">
+            <div class="input-group">
 
-            <input type="text" class="form-control mt-1" placeholder="search" name="search">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="submit" name="submit">Search</button>
+                <input type="text" class="form-control mt-1" placeholder="search" name="search">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit" name="submit">Search</button>
+                </div>
             </div>
-        </div>
-    </form>
-    <!-- /.input-group -->
+        </form>
+    </div>
 </div>
 
 <!-- Blog Categories Well -->
-<div class="well">
-    <h4>Blog Categories</h4>
-    <ul class="list-unstyled">
-        <?php
-        $query = "SELECT * FROM categories";
-        $select_all_categories_query = mysqli_query($connection, $query);
-        while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
-            $cat_title = $row['cat_title'];
-            ?>
-            <li>
-                <ahref="#"><?php echo $cat_title; ?></a>
-            </li>
+<div class="card">
+    <div class="card-body">
+        <h4>Blog Categories</h4>
+        <ul class="list-unstyled">
             <?php
-        }
-        ?>
-    </ul>
+            $query = "SELECT * FROM categories";
+            $select_all_categories_query = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                $cat_title = $row['cat_title'];
+                $cat_id = $row['cat_id'];
+                ?>
+                <li>
+                    <a href="category.php?category=<?php echo $cat_id; ?>"><?php echo $cat_title; ?></a>
+                </li>
+                <?php
+            }
+            ?>
+        </ul>
+    </div>
 </div>
 <!-- /.col-lg-6 -->
 
 <!-- /.col-lg-6 -->
 
 <!-- Side Widget Well -->
-<div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+<div class="card">
+    <div class="card-body">
+        <h4>Side Widget Well</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    </div>
 </div>
