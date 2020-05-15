bd5b4fef28ab42f687603064a103fa43ac9db1da
andonette <andi@madebyfactory.com>
2020-05-15 15:36:18 +0100

    tidy up navs

---
+++ admin/includes/side-nav.php
@@ -0,0 +1,55 @@
+<div class="sidebar" data="green" data-background="blue">
+    <div class="logo">
+        <a href="index.php" class="simple-text">
+            <img src="../images/dave.png" alt="" class="img-fluid pr-5">
+        </a>
+    </div>
+
+    <div class="sidebar-wrapper">
+        <ul class="nav">
+            <li>
+                <a href="posts.php">
+                    <i class="fal fa-pencil-alt"></i>
+                    <p>Posts</p>
+                </a>
+            </li>
+            <li>
+                <a href="categories.php">
+                    <i class="fal fa-folder-open"></i>
+                    <p>Categories</p>
+                </a>
+            </li>
+            <li>
+                <a href="comments.php">
+                    <i class="fal fa-comment-smile"></i>
+                    <p>Comments</p>
+                </a>
+            </li>
+            <li>
+                <a href="users.php">
+                    <i class="fal fa-users"></i>
+                    <p>Users</p>
+                </a>
+            </li>
+            <li>
+                <a href="profile.php">
+                    <i class="fal fa-user-circle"></i>
+                    <p>Profile</p>
+                </a>
+            </li>
+            <li>
+                <a href="./tables.html">
+                    <i class="fal fa-cog"></i>
+                    <p>Admin</p>
+                </a>
+            </li>
+            <li>
+                <a href="../">
+                    <i class="fal fa-desktop"></i>
+                    <p>Blog</p>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</div>
--- includes/header.php
+++ includes/header.php
@@ -21,41 +21,4 @@ session_start();
     <link rel="stylesheet" href="assets/css/blk-design-system-pro.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">Dave</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-bar navbar-kebab"></span>
-                <span class="navbar-toggler-bar navbar-kebab"></span>
-                <span class="navbar-toggler-bar navbar-kebab"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="dropdown nav-item">
-                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false">
-                            Categories</a>
-                            <div class="dropdown-menu dropdown-menu-right dropdown-danger">
-                                <?php
-                                $query = "SELECT * FROM categories";
-                                $select_all_categories = mysqli_query($connection, $query);
-                                while ($row = mysqli_fetch_assoc($select_all_categories)) {
-                                    $category_title = $row['cat_title'];
-                                    echo "<a class='dropdown-item'>{$category_title}</a>";
-                                }
-                                ?>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="admin/index.php">Admin</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link ml-auto" href="contact.php">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link ml-auto" href="registration.php">Register</a>
-                        </li>
-                        <?php show_update(); ?>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+<?php include 'nav.php' ?>
