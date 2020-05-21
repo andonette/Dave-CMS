c0d53434e1b4f7727b6f078c454c134a02c993a9
andonette <andi@madebyfactory.com>
2020-05-21 21:42:50 +0100

    saved category to temp file as new code doesn't work

--- category.php
+++ category.php
@@ -1,3 +1,4 @@
+
 <?php include 'includes/header.php' ?>
 <div class="container">
     <div class="row">
@@ -7,48 +8,42 @@
 
             if (isset($_GET['category'])) {
                 $get_category = $_GET['category'];
-                //query the database, select everything from the posts table
-                if (is_admin($_SESSION['username'])) {
-                    echo 'hi admin';
-                    $stmt1 = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE post_category_id = ?");
-                } else {
-                    $stmt2 = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE post_category_id = ? AND post_status = ?");
-                    $published = "Published";
-                }
-                if (isset($stmt1)) {
-                    echo 'stmt1';
-                    mysqli_stmt_bind_param($stmt1, "i", $post_category_id);
-                    mysqli_stmt_execute($stmt1);
-                    mysqli_stmt_bind_result($stmt1, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
-                    $stmt = $stmt1;
-                } else {
-                    mysqli_stmt_bind_param($stmt2, "is", $post_category_id);
-                    mysqli_stmt_execute($stmt2);
-                    mysqli_stmt_bind_result($stmt2, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
-                    $stmt = $stmt2;
-                }
+            //query the database, select everything from the posts table
+            //query the database, select everything from the posts table
+            if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'Administrator') {
+                $query = "SELECT * FROM posts WHERE post_category_id = $get_category";
+            } else {
+                $query = "SELECT * FROM posts WHERE post_category_id = $get_category AND post_status = 'Published'";
+            }
 
-                if (mysqli_stmt_num_rows($stmt) === 0) {
-                    echo '<h2>No Posts To Show</h2>';
-                    echo "<a class='btn btn-primary' href='index.php'>Back To Posts</a>";
-                } else {
-                    echo "<p>we found $count posts</p>";// code...
-                }
+            $select_all_posts_query = mysqli_query($connection, $query);
+            $count = mysqli_num_rows($select_all_posts_query);
 
-                //create variables from the columns in the post table.
-                while (mysqli_stmt_fetch($stmt1)) {
-                    echo 'hi';
-                    include 'includes/post-loop-template.php';
-                }
-            } ?>
-            </div>
-            <!-- End Column -->
-            <div class="col-sm-4">
-                <?php include 'includes/sidebar.php' ?>
-            </div>
-            <!-- End Column -->
+            if ($count !== 0) {
+                echo "<p>we found $count posts</p>";// code...
+            } else {
+                echo '<h2>No Posts To Show</h2>';
+                echo "<a class='btn btn-primary' href='index.php'>Back To Posts</a>";
+            }
+
+            //create variables from the columns in the post table.
+            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                $post_id = $row['post_id'];
+                $post_title = $row['post_title'];
+                $post_author = $row['post_author_id'];
+                $post_date = date_create($row['post_date']);
+                $post_image = $row['post_image'];
+                $post_content = $row['post_content'];
+                include 'includes/post-loop-template.php';
+            } } ?>
         </div>
-        <!-- End Row -->
+        <!-- End Column -->
+        <div class="col-sm-4">
+            <?php include 'includes/sidebar.php' ?>
+        </div>
+        <!-- End Column -->
     </div>
-    <!-- End Container -->
-    <?php include 'includes/footer.php' ?>
+    <!-- End Row -->
+</div>
+<!-- End Container -->
+<?php include 'includes/footer.php' ?>
---
+++ category2.php
@@ -0,0 +1,57 @@
+<?php include 'includes/header.php' ?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8">
+            <h1 class="title">Latest Blogposts</h1>
+            <?php
+
+            if (isset($_GET['category'])) {
+                global $connection;
+                $get_category = $_GET['category'];
+                //query the database, select everything from the posts table
+                if (is_admin($_SESSION['username'])) {
+                    echo 'stmt1 being used<br>';
+                    $stmt1 = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE post_category_id = ?");
+                } else {
+                    $stmt2 = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE post_category_id = ? AND post_status = ?");
+                    $published = "Published";
+                    echo 'stmt2 being used<br>';
+                }
+                if (isset($stmt1)) {
+                    echo 'stmt1 isset<br>';
+                    mysqli_stmt_bind_param($stmt1, "i", $post_category_id);
+                    mysqli_stmt_execute($stmt1);
+                    mysqli_stmt_bind_result($stmt1, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
+                    $stmt = $stmt1;
+                } else {
+                    echo 'stmt2 isset<br>';
+                    mysqli_stmt_bind_param($stmt2, "is", $post_category_id);
+                    mysqli_stmt_execute($stmt2);
+                    mysqli_stmt_bind_result($stmt2, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
+                    $stmt = $stmt2;
+                }
+
+                if (mysqli_stmt_num_rows($stmt) === 0) {
+                    echo '<h2>No Posts To Show</h2>';
+                    echo "<a class='btn btn-primary' href='index.php'>Back To Posts</a>";
+                } else {
+                    echo "<p>we found $count posts</p>";// code...
+                }
+
+                //create variables from the columns in the post table.
+                while (mysqli_stmt_fetch($stmt)) {
+                    echo 'working';
+                    include 'includes/post-loop-template.php';
+                }
+            } ?>
+            </div>
+            <!-- End Column -->
+            <div class="col-sm-4">
+                <?php include 'includes/sidebar.php' ?>
+            </div>
+            <!-- End Column -->
+        </div>
+        <!-- End Row -->
+    </div>
+    <!-- End Container -->
+    <?php include 'includes/footer.php' ?>
