a9a033b7df7667e80cda6797060456fea537669b
andonette <andi@madebyfactory.com>
2020-05-21 21:59:34 +0100

    rewrite rule for static pages

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="index.php">Dave</a>
+        <a class="navbar-brand" href="/Dave-CMS">Dave</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-bar navbar-kebab"></span>
             <span class="navbar-toggler-bar navbar-kebab"></span>
@@ -20,7 +20,7 @@
                                 $cat_id = $row['cat_id'];
                                 $category_class = '';
                                 $registration_class = '';
-                                $registration = 'registration.php';
+                                $registration = 'registration';
                                 $current_page = basename($_SERVER['PHP_SELF']);
                                 if (isset($_GET['category']) && $_GET['category'] == $cat_id) {
                                     $category_class = 'active';
@@ -33,10 +33,10 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link ml-auto  <?php echo $registration_class ?>" href="contact.php">Contact</a>
+                        <a class="nav-link ml-auto  <?php echo $registration_class ?>" href="contact">Contact</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link ml-auto <?php echo $registration_class ?>" href="registration.php">Register</a>
+                        <a class="nav-link ml-auto <?php echo $registration_class ?>" href="registration">Register</a>
                     </li>
                     <?php show_update(); ?>
                 </ul>
